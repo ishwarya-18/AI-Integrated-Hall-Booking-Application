971e349f163c37e555352fa19a9850f61e0d9792
Ishwarya R <ishwarya.ad23@bitsathy.ac.in>
2026-02-04 18:52:10 +0530

    Revert "Initial commit of SmartVenue Android mobile app"
    This reverts commit c5e0830d52600955a8e6bbfbdab0ba421776f731.

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.hallbooking',
-  appName: 'vite_react_shadcn_ts',
-  webDir: 'dist'
-};
-
-export default config;
